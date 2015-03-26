c471b73dc919ea7cb2d4a5f6f6276a3209e287b0
Chaim Oropeza Garcia <chaim.oropeza@gmail.com>
2015-03-26 09:47:57 -0600

    Initial commit

---
+++ app/src/main/java/com/labene/lanconta/activosfijosmobile/Splash_Activity.java
@@ -0,0 +1,50 @@
+package com.labene.lanconta.activosfijosmobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Chaim on 24/03/2015.
+ */
+public class Splash_Activity extends Activity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash_activity);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        Splash_Activity.this, MainActivity.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+
+}
